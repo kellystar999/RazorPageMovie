f75541f6b9e159533ff5d112a6414e80c8734a53
SOOS <PC@DESKTOP-84SIJTJ.(none)>
2024-08-15 04:37:52 +0900

    Feat:update

--- obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b931476985bfec0bbaedea42f387655088e5b98a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc5d763b5e4797dbf7ed79fd51f8141539901fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
